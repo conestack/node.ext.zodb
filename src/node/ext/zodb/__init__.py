f52a750e8e9426a8f196073afb738a12053f6721
Robert Niederreiter <rnix@squarewave.at>
2011-05-06 12:16:10 +0200

    provide odict and node implementation using OOBTree

--- src/node/ext/zodb/__init__.py
+++ src/node/ext/zodb/__init__.py
@@ -4,13 +4,20 @@ from plumber import (
     Part,
     default,
     extend,
+    finalize,
 )
-from zope.interface import implements
-from odict.pyodict import _odict
+from odict.pyodict import (
+    _odict,
+    _nil,
+)
 from persistent import Persistent
 from persistent.dict import PersistentDict
 from BTrees.OOBTree import OOBTree
-from node.interfaces import IStorage
+from zope.interface import implements
+from node.interfaces import (
+    INode,
+    IAttributes,
+)
 from node.parts import (
     Adopt,
     Nodespaces,
@@ -20,12 +27,9 @@ from node.parts import (
     DefaultInit,
     Nodify,
     NodeChildValidate,
+    Storage,
 )
-
-#class Podict(_odict, OOBTree):
-#    
-#    def _dict_impl(self):
-#        return OOBTree
+from node.utils import AttributeAccess
 
 
 class Podict(_odict, PersistentDict):
@@ -34,8 +38,59 @@ class Podict(_odict, PersistentDict):
         return PersistentDict
 
 
-class PodictStorage(Part):
-    implements(IStorage)
+class OOBTodict(_odict, OOBTree):
+    
+    def _dict_impl(self):
+        return OOBTree
+    
+    # Double-linked list header
+    def _get_lh(self):
+        try:
+            return self['____lh']
+        except KeyError:
+            self['____lh'] = _nil
+        return self['____lh']
+
+    def _set_lh(self, val):
+        self['____lh'] = val
+
+    lh = property(_get_lh, _set_lh)
+
+    # Double-linked list tail
+    def _get_lt(self):
+        try:
+            return self['____lt']
+        except KeyError:
+            self['____lt'] = _nil
+        return self['____lt']
+
+    def _set_lt(self, val):
+        self['____lt'] = val
+
+    lt = property(_get_lt, _set_lt)
+    
+    def __getitem__(self, key):
+        if key.startswith('____'):
+            return self._dict_impl().__getitem__(self, key)
+        return _odict.__getitem__(self, key)
+    
+    def __setitem__(self, key, val):
+        if key.startswith('____'):
+            # private attributes, no way to set persistent attributes on
+            # OOBTree deriving class.
+            self._dict_impl().__setitem__(self, key, val)
+        else:
+            _odict.__setitem__(self, key, val)
+    
+    def __repr__(self):
+        if self:
+            pairs = ("(%r, %r)" % (k, v) for k, v in self.iteritems())
+            return "OOBTodict([%s])" % ", ".join(pairs)
+        else:
+            return "OOBTodict()"
+
+
+class PodictStorage(Storage):
     
     @default
     @property
@@ -43,37 +98,19 @@ class PodictStorage(Part):
         if not hasattr(self, '_storage_data'):
             self._storage_data = Podict()
         return self._storage_data
-    
-    @extend
-    def __getitem__(self, key):
-        return self.storage[key]
-    
-    @extend
-    def __delitem__(self, key):
-        del self.storage[key]
-    
-    @extend
-    def __setitem__(self, key, val):
-        self.storage[key] = val
-    
-    @extend
-    def __iter__(self):
-        return self.storage.__iter__()
 
 
-class ZODBNode(Persistent):
-    __metaclass__ = plumber
-    __plumbing__ = (
-        NodeChildValidate,
-        Nodespaces,
-        Adopt,
-        Attributes,
-        Order,
-        AsAttrAccess,
-        DefaultInit,
-        Nodify,
-        PodictStorage,
-    )
+class OOBTodictStorage(Storage):
+    
+    @default
+    @property
+    def storage(self):
+        if not hasattr(self, '_storage_data'):
+            self._storage_data = OOBTodict()
+        return self._storage_data
+
+
+class ZODBPart(Part):
     
     def _get_parent(self):
         return self._v_parent
@@ -81,12 +118,95 @@ class ZODBNode(Persistent):
     def _set_parent(self, val):
         self._v_parent = val
     
-    __parent__ = property(_get_parent, _set_parent)
+    __parent__ = extend(property(_get_parent, _set_parent))
     
+    @extend
     def __getitem__(self, key):
         val = self.storage[key]
-        val.__parent__ = self
+        if INode.providedBy(val):
+            val.__parent__ = self
         return val
     
+    @extend
     def __call__(self):
         transaction.commit()
+
+
+class ZODBAttributes(Part):
+    implements(IAttributes)
+    
+    attribute_access_for_attrs = default(False)
+    attributes_factory = default(None)
+
+    @finalize
+    @property
+    def attrs(self):
+        try:
+            attrs = self._attrs
+        except AttributeError:
+            self._attrs = attrs = self.attributes_factory(name='_attrs',
+                                                          parent=self)
+        if self.attribute_access_for_attrs:
+            return AttributeAccess(attrs)
+        return attrs
+    
+    # BBB
+    attributes = finalize(attrs)
+
+
+class ZODBNodeAttributes(Persistent):
+    __metaclass__ = plumber
+    __plumbing__ = (
+        NodeChildValidate,
+        Adopt,
+        DefaultInit,
+        Nodify,
+        ZODBPart,
+        PodictStorage,
+    )
+    allow_non_node_childs = True
+
+
+class ZODBNode(Persistent):
+    __metaclass__ = plumber
+    __plumbing__ = (
+        NodeChildValidate,
+        Adopt,
+        ZODBAttributes,
+        Order,
+        AsAttrAccess,
+        DefaultInit,
+        Nodify,
+        ZODBPart,
+        PodictStorage,
+    )
+    attributes_factory = ZODBNodeAttributes
+   
+    
+class OOBTNodeAttributes(Persistent):
+    __metaclass__ = plumber
+    __plumbing__ = (
+        NodeChildValidate,
+        Adopt,
+        DefaultInit,
+        Nodify,
+        ZODBPart,
+        OOBTodictStorage,
+    )
+    allow_non_node_childs = True
+
+
+class OOBTNode(Persistent):
+    __metaclass__ = plumber
+    __plumbing__ = (
+        NodeChildValidate,
+        Adopt,
+        ZODBAttributes,
+        Order,
+        AsAttrAccess,
+        DefaultInit,
+        Nodify,
+        ZODBPart,
+        OOBTodictStorage,
+    )
+    attributes_factory = OOBTNodeAttributes
