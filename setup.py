3ff69b839b0747e1250a2a281dd809ec55974b75
Robert Niederreiter <rnix@squarewave.at>
2011-12-13 16:03:36 +0100

    0.9pre1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.9'
+version = '0.9pre1'
 shortdesc = 'node.ext.zodb'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
